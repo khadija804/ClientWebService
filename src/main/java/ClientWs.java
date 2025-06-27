ee6d488d6b62bb8fb9ffa8bae178ea2f0773f0c2
khadija804 <khadija.agoujim17@gmail.com>
2025-06-27 12:26:59 +0100

    add readme

--- src/main/java/ClientWs.java
+++ src/main/java/ClientWs.java
@@ -6,9 +6,9 @@ import proxy.Compte;
 public class ClientWs {
     public static void main(String[] args) {
         BanqueService stub = new BanqueWS().getBanqueServicePort();
-        System.out.println(stub.convert(78000));
+        System.out.println( "Convertir le montant 7800 en euros :" + stub.convert(7800));
         Compte compte = stub.getCompte(5);
-        System.out.println(compte.getCode());
-        System.out.println(compte.getSolde());
+        System.out.println(" le compte ayant le code : "+compte.getCode());
+        System.out.println(" le solde du compte est "+ compte.getSolde());
     }
 }
